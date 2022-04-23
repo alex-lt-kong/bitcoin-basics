a0450379b8513d1484e3ae2eb761c6a084cc3423
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-04-23 18:04:27 +0800

    ch1 finished

--- 01_finite-field/main.cpp
+++ 01_finite-field/main.cpp
@@ -13,6 +13,7 @@ class FieldElement {
     FieldElement operator*(const FieldElement& other);
     FieldElement operator/(const FieldElement& other);
     FieldElement power(int exponent);
+    int modulusPower(int exponent, int modulus);
     string toString();
 };
 
@@ -54,20 +55,29 @@ FieldElement FieldElement::operator/(const FieldElement& other)
   if (this->prime != other.prime) {
     throw std::invalid_argument("prime numbers are different");
   }
-  return FieldElement((long long int)(this->num * pow(other.num, this->prime-2)) % this->prime, this->prime);
+  int tmp = FieldElement(other.num, other.prime).power(other.prime-2).num;
+  return FieldElement(
+    (this->num * tmp) % this->prime,
+    this->prime
+  );
+}
+
+int FieldElement::modulusPower(int exponent, int modulus)
+{
+  int n = exponent;
+  int result = 1;
+  while (n < 0) { n += modulus - 1; };
+  // C++ -7 % 3 = -1; python: -7 % 3 = 2;
+  for (int i = 0; i < n; i++) {
+    result *= this->num;
+    result = result % modulus;
+  }
+  return result;
 }
 
 FieldElement FieldElement::power(int exponent)
 {
-  int n = exponent;
-  int result = 1;
-  while (n < 0) { n += this->prime - 1; };
-  // C++ -7 % 3 = -1; python: -7 % 3 = 2;
-  for (int i = 0; i < n; i++) {
-    result *= this->num;
-    result = result % this->prime;
-  }
-  return FieldElement(result, this->prime);
+  return FieldElement(this->modulusPower(exponent, this->prime), this->prime);
 }
 
 string FieldElement::toString() {
@@ -77,25 +87,30 @@ string FieldElement::toString() {
 void testDivision() {
   FieldElement *a = new FieldElement(7, 19);
   FieldElement b = FieldElement(5, 19);
-  cout << ((*a)/b).toString() << endl;
+  cout << "Test division:\n"
+       << (FieldElement(2, 19)/FieldElement(7, 19)).toString() << "\n"
+       << ((*a)/b).toString() << endl;
   delete a;
 }
 
 void testPower() {
   FieldElement *a = new FieldElement(3, 13);
   FieldElement b = FieldElement(1, 13);
-  cout << ((*a).power(3) == b) << endl;
+  cout << "Test power:\n"
+      << ((*a).power(3) == b) << "\n"
+      << (FieldElement(95, 97) * FieldElement(45, 97) * FieldElement(31, 97)).toString()
+      << endl;
   delete a;
 }
 
 void testAddition() {
-  FieldElement *a = new FieldElement(17,  19);
-  FieldElement b = FieldElement(12, 19);
-  FieldElement c = FieldElement(8,  19);
+  FieldElement *a = new FieldElement(7,  13);
+  FieldElement b = FieldElement(12, 13);
+  FieldElement c = FieldElement(6,  13);
   // We can define an object in two ways.
   // The 1st way, heap memory is used, we need to take care of it ourselves
   // The 2nd way, stack memory is used, the object will be destoryed automatically. 
-  cout << (*a + b == c) << endl;
+  cout << "Test addition:\n" << (*a + b == c) << endl;
   cout << (c * (*a)).toString() << endl;
 
   delete a;
@@ -105,6 +120,7 @@ int main() {
   testAddition();
   testPower();
   testDivision();
+  cout << "Exercise 8" << endl;
   cout << (FieldElement(3, 31)/FieldElement(24, 31)).toString() << endl;
   cout << (FieldElement(17, 31).power(-3)).toString() << endl;
   cout << (FieldElement(4, 31).power(-4)*FieldElement(11, 31)).toString() << endl;
