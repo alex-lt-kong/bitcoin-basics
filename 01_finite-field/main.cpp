a21a2bfa78af51223c4c870b7be03255ac8eccc4
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-04-24 12:40:13 +0800

    separate class to header files

--- 01_finite-field/main.cpp
+++ 01_finite-field/main.cpp
@@ -1,88 +1,9 @@
 #include <iostream>
 #include <stdexcept>
 #include <math.h>
+#include "field-element.h"
 
 using namespace std;
-
-class FieldElement {
-  public:
-    int num, prime;
-    FieldElement(int,int);
-    bool operator==(const FieldElement& other) const;
-    FieldElement operator+(const FieldElement& other);
-    FieldElement operator*(const FieldElement& other);
-    FieldElement operator/(const FieldElement& other);
-    FieldElement power(int exponent);
-    int modulusPower(int exponent, int modulus);
-    string toString();
-};
-
-FieldElement::FieldElement(int num, int prime) {
-  if (num >= prime || num < 0) {
-    throw std::invalid_argument(
-      "invalid num: " + to_string(num) + ". Either it is negative or greater than prime"
-    );
-  }
-  this->num = num;
-  this->prime = prime;
-}
-
-bool FieldElement::operator==(const FieldElement& other) const
-{
-  // ICYW: This is overloading, not overriding lol
-  return this->prime == other.prime && this->num == other.num;
-}
-
-FieldElement FieldElement::operator+(const FieldElement& other)
-{
-  // ICYW: This is overloading, not overriding lol
-  if (this->prime != other.prime) {
-    throw std::invalid_argument("prime numbers are different");
-  }
-  return FieldElement((this->num + other.num) % this->prime, this->prime);
-}
-
-FieldElement FieldElement::operator*(const FieldElement& other)
-{
-  if (this->prime != other.prime) {
-    throw std::invalid_argument("prime numbers are different");
-  }
-  return FieldElement((this->num * other.num) % this->prime, this->prime);
-}
-
-FieldElement FieldElement::operator/(const FieldElement& other)
-{
-  if (this->prime != other.prime) {
-    throw std::invalid_argument("prime numbers are different");
-  }
-  int tmp = FieldElement(other.num, other.prime).power(other.prime-2).num;
-  return FieldElement(
-    (this->num * tmp) % this->prime,
-    this->prime
-  );
-}
-
-int FieldElement::modulusPower(int exponent, int modulus)
-{
-  int n = exponent;
-  int result = 1;
-  while (n < 0) { n += modulus - 1; };
-  // C++ -7 % 3 = -1; python: -7 % 3 = 2;
-  for (int i = 0; i < n; i++) {
-    result *= this->num;
-    result = result % modulus;
-  }
-  return result;
-}
-
-FieldElement FieldElement::power(int exponent)
-{
-  return FieldElement(this->modulusPower(exponent, this->prime), this->prime);
-}
-
-string FieldElement::toString() {
-  return to_string(this->num) + " (" +to_string(this->prime)  + ")";
-}
 
 void testDivision() {
   FieldElement *a = new FieldElement(7, 19);
---
+++ 03_elliptic-curve-cryptography/main.cpp
@@ -0,0 +1,111 @@
+#include <float.h>
+#include <iostream>
+#include <stdexcept>
+#include <limits.h>
+#include <math.h>
+
+#include "../01_finite-field/main.cpp"
+
+using namespace std;
+
+class Point {
+  public:
+    float a = 0;
+    float b = 0;
+    float x = 0;
+    float y = 0;
+    Point(float x, float y, float a, float b);
+    bool operator==(const Point& other) const;
+    Point operator+(const Point& other);
+    string toString();
+};
+
+Point::Point(float x, float y, float a, float b) {
+  this->a = a;
+  this->b = b;
+  this->x = x;
+  this->y = y;
+  
+  if (this->x == FLT_MAX && this->y == FLT_MAX) { return; }
+
+  if (pow(y, 2) != pow(x, 3) + a * x + b) {
+    throw std::invalid_argument(
+      "(" + to_string(x) + ", " + to_string(y) + ") is not on the curve"
+    );
+  }
+
+}
+
+bool Point::operator==(const Point& other) const
+{
+  ;
+  // ICYW: This is overloading, not overriding
+  return (
+    this->a == other.a && this->b == other.b && 
+    this->x == other.x && this->y == other.y
+  );
+}
+
+Point Point::operator+(const Point& other)
+{
+  // ICYW: This is overloading, not overriding lol
+  if (*this == other || this->y == 0) {
+    return Point(FLT_MAX, FLT_MAX, this->a, this->b);
+  }
+  if (this->a != other.a || this->b != other.b) {
+    throw std::invalid_argument("Two points are not on the same curve");
+  }
+  // FLT_MAX represents infinity
+  if (this->x == FLT_MAX) { return other; }
+  if (other.x == FLT_MAX) { return *this; }
+  if (this->x == other.x && this->y != other.y) {
+    return Point(FLT_MAX, FLT_MAX, this->a, this->b);//point at infinity
+  }
+
+  float slope = 0;
+  if (this->x == other.x && this->y == other.y) {
+    // P1 == P2, need some calculus to derive this formula
+    slope = (3 * this->x * this->x + this->a) / (this->y * 2);
+  } else {
+    // general case
+    slope = (other.y - this->y) / (other.x - this->x);
+  }
+  float x3 = slope * slope - this->x - other.x;
+  float y3 = slope * (this->x - x3) - this->y;
+  return Point(x3, y3, this->a, this->b);
+}
+
+string Point::toString() {
+  return "Point(" + (this->x == FLT_MAX ? "Inf" : to_string(this->x)) + ", "
+                  + (this->y == FLT_MAX ? "Inf" : to_string(this->y)) + ")_"
+                  + to_string(this->a) + "_" + to_string(this->b);
+}
+
+void testPointAdditionInfinity() {
+  Point p1 = Point(-1, -1, 5, 7);
+  Point p2 = Point(-1, 1, 5, 7);
+  Point pInf = Point(FLT_MAX, FLT_MAX, 5, 7);
+  cout << "testPointAdditionInfinity:\n"
+       << (p1 + pInf).toString() << "\n"
+       << (p2 + pInf).toString() << "\n"
+       << (p1 + p2).toString() << endl;
+  
+}
+
+void testPointAddinitionDifferentXs() {
+  cout << "testPointAdditionInfinity:\n"
+       << (Point(2, 5, 5, 7) + Point(-1, -1, 5, 7)).toString()
+       << endl;
+}
+
+
+void testPointAddinitionSamePoint() {
+  cout << "testPointAddinitionSamePoint:\n"
+       << (Point(-1, -1, 5, 7) + Point(-1, -1, 5, 7)).toString()
+       << endl;
+}
+
+int main() {
+
+  return 0;
+}
