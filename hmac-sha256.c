cd1cde5589d481730a735b2af7e7cef3fb7114a7
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-05-16 13:58:36 +0800

    revision

--- hmac-sha256.c
+++ hmac-sha256.c
@@ -14,44 +14,40 @@
 static void* H(const void* x, const size_t xlen, const void* y, const size_t ylen, void* out);
 
 
-void hmac_sha256(const void* key, const size_t keylen, const void* data, const size_t datalen, void* out) {
-  uint8_t k[SHA256_BLOCK_SIZE];
-  uint8_t k_ipad[SHA256_BLOCK_SIZE];
-  uint8_t k_opad[SHA256_BLOCK_SIZE];
+void hmac_sha256(const void* key, const size_t key_len, const void* msg, const size_t msg_len, void* out) {
+  uint8_t k_prime[SHA256_BLOCK_SIZE];
+  uint8_t ipad[SHA256_BLOCK_SIZE]; // ipad is the block-sized inner padding, consisting of repeated bytes valued 0x36
+  uint8_t opad[SHA256_BLOCK_SIZE]; // opad is the block-sized outer padding, consisting of repeated bytes valued 0x5c
   uint8_t ihash[SHA256_HASH_SIZE];
   uint8_t ohash[SHA256_HASH_SIZE];
   int i;
 
-  memset(k, 0, sizeof(k));
-  memset(k_ipad, 0x36, SHA256_BLOCK_SIZE);
-  memset(k_opad, 0x5c, SHA256_BLOCK_SIZE);
+  memset(k_prime, 0, sizeof(k_prime));
+  memset(ipad, 0x36, SHA256_BLOCK_SIZE);
+  memset(opad, 0x5c, SHA256_BLOCK_SIZE);
 
-  if (keylen > SHA256_BLOCK_SIZE) {
+  if (key_len > SHA256_BLOCK_SIZE) {
     // If the key is larger than the hash algorithm's
     // block size, we must digest it first.
-    calc_sha_256(k, key, keylen);
+    calc_sha_256(k_prime, key, key_len);
   } else {
-    memcpy(k, key, keylen);
+    memcpy(k_prime, key, key_len);
   }
 
-  for (i = 0; i < SHA256_BLOCK_SIZE; i++) {
-    k_ipad[i] ^= k[i];
-    k_opad[i] ^= k[i];
+  for (int i = 0; i < SHA256_BLOCK_SIZE; i++) {
+    ipad[i] ^= k_prime[i];
+    opad[i] ^= k_prime[i];
   }
 
   // Perform HMAC algorithm: ( https://tools.ietf.org/html/rfc2104 )
   //      `H(K XOR opad, H(K XOR ipad, data))`
-  H(k_ipad, sizeof(k_ipad), data, datalen, ihash);
-  H(k_opad, sizeof(k_opad), ihash, sizeof(ihash), ohash);
+  H(ipad, sizeof(ipad), msg, msg_len, ihash);
+  H(opad, sizeof(opad), ihash, sizeof(ihash), ohash);
 
   memcpy(out, ohash, SHA256_HASH_SIZE);
 }
 
-static void* H(const void* x,
-               const size_t xlen,
-               const void* y,
-               const size_t ylen,
-               void* out) {
+static void* H(const void* x, const size_t xlen, const void* y, const size_t ylen, void* out) {
   size_t buflen = (xlen + ylen);
   uint8_t* buf = (uint8_t*)malloc(buflen);
 
