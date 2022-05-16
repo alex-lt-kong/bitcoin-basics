115f281d7e733d6bc2395ec8dbfedbf2deb2b242
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-05-16 15:37:43 +0800

    revision

--- hmac-sha256.c
+++ hmac-sha256.c
@@ -15,11 +15,11 @@ static void* H(const void* x, const size_t xlen, const void* y, const size_t yle
 
 
 void hmac_sha256(const void* key, const size_t key_len, const void* msg, const size_t msg_len, void* out) {
-  uint8_t k_prime[SHA256_BLOCK_SIZE];
-  uint8_t ipad[SHA256_BLOCK_SIZE]; // ipad is the block-sized inner padding, consisting of repeated bytes valued 0x36
-  uint8_t opad[SHA256_BLOCK_SIZE]; // opad is the block-sized outer padding, consisting of repeated bytes valued 0x5c
-  uint8_t ihash[SHA256_HASH_SIZE];
-  uint8_t ohash[SHA256_HASH_SIZE];
+  unsigned char k_prime[SHA256_BLOCK_SIZE];
+  unsigned char ipad[SHA256_BLOCK_SIZE]; // ipad is the block-sized inner padding, consisting of repeated bytes valued 0x36
+  unsigned char opad[SHA256_BLOCK_SIZE]; // opad is the block-sized outer padding, consisting of repeated bytes valued 0x5c
+  unsigned char ihash[SHA256_HASH_SIZE];
+  unsigned char ohash[SHA256_HASH_SIZE];
   int i;
 
   memset(k_prime, 0, sizeof(k_prime));
@@ -44,7 +44,7 @@ void hmac_sha256(const void* key, const size_t key_len, const void* msg, const s
 
 static void* H(const void* x, const size_t xlen, const void* y, const size_t ylen, void* out) {
   size_t buflen = (xlen + ylen);
-  uint8_t* buf = (uint8_t*)malloc(buflen);
+  unsigned char* buf = (unsigned char*)malloc(buflen);
 
   memcpy(buf, x, xlen);
   memcpy(buf + xlen, y, ylen);
