fd8b60ace2f6beb3a14589e527a49b358fd9e9f3
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-12-04 18:35:20 +0800

    minor updat4

--- op.h
+++ op.h
@@ -27,8 +27,7 @@ bool op_hash256(stack<vector<uint8_t>>* data_stack) {
   cal_sha256_hash(ele.data(), ele.size(), hash_bytes);
   cal_sha256_hash(hash_bytes, SHA256_HASH_SIZE, hash_bytes);
   vector<uint8_t> hash(SHA256_HASH_SIZE);
-  memcpy(&hash[0], &hash_bytes[0], SHA256_HASH_SIZE * sizeof(uint8_t));
-  // fill a pre-allocated vector<uint8_t> with a byte array
+  memcpy(hash.data(), hash_bytes, SHA256_HASH_SIZE * sizeof(uint8_t));
   data_stack->push(hash);
   return true;
 }
@@ -42,8 +41,7 @@ bool op_hash160(stack<vector<uint8_t>>* data_stack) {
   uint8_t hash_bytes[RIPEMD160_HASH_SIZE];
   hash160(ele.data(), ele.size(), hash_bytes);
   vector<uint8_t> hash(RIPEMD160_HASH_SIZE);
-  memcpy(&hash[0], &hash_bytes[0], RIPEMD160_HASH_SIZE * sizeof(uint8_t));
-  // fill a pre-allocated vector<uint8_t> with a byte array
+  memcpy(hash.data(), hash_bytes, RIPEMD160_HASH_SIZE * sizeof(uint8_t));
   data_stack->push(hash);
   return true;
 }
