8df0af0ef3e0b921db7beb312860c19c3a46a620
Alex Kong <alex-lt-kong@connect.ust.hk>
2022-12-20 08:18:10 +0800

    more opcode

--- src/op.cpp
+++ src/op.cpp
@@ -99,13 +99,16 @@ OpFuncStruct get_opcode(size_t op_id) {
   Ops[113] = (OpFuncStruct){"OP_2ROT",             &op_notimplemented};
   Ops[117] = (OpFuncStruct){"OP_DROP",             &op_notimplemented};
   Ops[118] = (OpFuncStruct){"OP_DUP",              &op_dup};
-  Ops[119] = (OpFuncStruct){"OP_NIP",              &op_notimplemented};
+  Ops[119] = (OpFuncStruct){"OP_NIP",              &op_notimplemented};  
+  Ops[122] = (OpFuncStruct){"OP_ROLL",             &op_notimplemented};
   Ops[123] = (OpFuncStruct){"OP_ROT",              &op_notimplemented};
   Ops[125] = (OpFuncStruct){"OP_TUCK",             &op_notimplemented};    
   Ops[127] = (OpFuncStruct){"OP_SUBSTR",           &op_notimplemented};
   Ops[126] = (OpFuncStruct){"OP_CAT",              &op_notimplemented}; 
   Ops[131] = (OpFuncStruct){"OP_INVERT",           &op_notimplemented};
   Ops[132] = (OpFuncStruct){"OP_AND",              &op_notimplemented};
+  Ops[133] = (OpFuncStruct){"OP_OR",               &op_notimplemented};
+  Ops[134] = (OpFuncStruct){"OP_XOR",              &op_notimplemented};
   Ops[135] = (OpFuncStruct){"OP_EQUAL",            &op_notimplemented};
   Ops[136] = (OpFuncStruct){"OP_EQUALVERIFY",      &op_notimplemented};
   Ops[137] = (OpFuncStruct){"OP_RESERVED1",        &op_notimplemented};
